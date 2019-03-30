d5e73d591cffee829d4c63f9cf35e8792b562df2
Ichi <ichibunny@hotmail.com>
2019-03-30 22:35:27 +0000

    2-3 wip

--- 2-3.c
+++ 2-3.c
@@ -29,10 +29,11 @@ https://www.tutorialspoint.com/c_standard_library/c_function_isxdigit.htm
 */
 
 int hextoi (char str1 []);
+int is_hex (char str1[]);
 
 int main(){
     
-    char str1 [] = "\xita3";
+    char str1 [] = "0xa3";
     
     printf ("%d \n", hextoi(str1));
     
@@ -42,31 +43,30 @@ int main(){
 
 // create second finction first
 // both char is hexdesmo without the prefix of indication. this will befinally changed once is_hex function created
-
+// 
 int hextoi (char str1 []){
     int i;
     int len = strlen (str1);
-    printf ("%s \n", str1);
-    for (i = 0; i < len; i++){
-        printf ("%c \n", str1[i]);
-    }   
-    printf ("%d , \n", len);
-
-    i = 0;
     int j = 0;
-
     int add_all = 0;
 
-    for (i = 0; i < len; i++){
+    if ((i = is_hex(str1) == 0){
+        printf ("This is not hexdecimol");
+    }
+    
+    for (i = is_hex(str1); i < len; i++){
         /* convert from ascii to real number if it is 1-9 (str1 [i] -48) */
         if (isdigit(str1[i])){
             j = str1[i] - 48;
         }
-        else {
+        else if {
             /* to simplyfy, make all lower case and convert to hex number in decimol*/
             j =  tolower (str1[i]);
             /* convert a-f to 10-15 */
             j = j - 87;
+        }
+        else {
+            printf ("this van not be valid hexdecimol. It contents value other than 1-9 and a-f or A-F");
         }
         /* convert what it should be power of -len-i-1*/
         add_all += j * pow (16, len-i-1);
@@ -77,25 +77,25 @@ int hextoi (char str1 []){
      return add_all;
 }
 
+int is_hex (char str1[]){
 
-/*
-char is_hex (char str1[]){
-
-    int i = 0;
+    //int i =0;
     int j = 0;
-
+    /*
     if (str1 [0]== '\x'){
         j = 1;
-    } else if (str1[0] == '0' && (str1[1]== 'x' || str1[1]== 'X')){
-        j = 2;
+
+        // will need impliment ssome exception and error handler
+    } else */
+    if (str1[0] == '0' && (str1[1]== 'x' || str1[1]== 'X')){
+        return j;
     }
     else {
-        return 
+        return 0;
     }
 
-
 }
-*/ 
+
 
 
 
