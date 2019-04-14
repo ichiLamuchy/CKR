142df6947d77741e1e97877a63c86ee82e17f4f8
ichiee <iichimura@fastly.com>
2019-04-14 18:18:36 +0100

    Remove redandant code

--- 2-5.c
+++ 2-5.c
@@ -9,33 +9,25 @@ contains no characters from s2. (The standard library function strpbrk does
 the same job but returns a pointer to the location.
 */
 
-#define LIM 10
-#define MAX 20
+#define LIM 20
 
 int get_line1(char a[], char b[]);
 int c_any (char str1[], char str2[]);
 
 int main(){
 
-    int i;
     char ori_str[LIM];
     char match_str[LIM];
-
     get_line1(ori_str, match_str);
-    i = c_any(ori_str, match_str);
 
-    printf ("1st index of matched char on string 1 is :  %d \n", i);
+    printf ("1st index of matched char on string 1 is :  %d \n",  c_any(ori_str, match_str));
 
-
+    return 0;
 }
 
 int c_any (char str1[], char str2[]){
 
-    int i, j, k, l, found;
-
-    // cast to int from unsigned long
-    j = (int)strlen(str1);
-    l = (int)strlen(str2);
+    int i, k, found;
     found = 0;  /* flag for string match found */
 
         /* check each character on str1 until terminator */
@@ -43,27 +35,15 @@ int c_any (char str1[], char str2[]){
             /* check each characotor on s2 until teminator */
             for (k = 0; str2[k] != '\0'; k++){
 
-                /* if 1st matched - get i value on j so that next round it will be copy over on jth index */
+                /* if 1st matched - return i  */
                 if (str1[i] == str2[k] && found == 0){
                     found = 1;
                     return i;
                 } 
-                /*
-                testing if it needs
-                */
-                else if (str1[i] != str2[k] && (i == j-1) && (k == l-1)){                    
-                    str1[j++] = str1[i];
-                }
             }
         }
-    if (found !=1){
-        return -1;
-    }
-    return 0;        
+    return -1;        
 }
-
-
-
 
 /* 
     getLine:     read a line return int - the length 
@@ -83,14 +63,13 @@ int get_line1(char a[], char b[]){
     a[i] = '\0';
 
     printf("Original Str: %s \n", a);
-
     printf ("Please type the removing chars \n");
+
     for (i = 0; i < LIM - 1 && (k = getchar()) != EOF && k != '\n'; i++){       
         b[i] = k;
     }
     
     printf("Removing Str: %s \n", b);
-
     return i;
 }
 
