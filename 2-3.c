ddc48ed1d59a072e58fa1c68cd09b992a6e21231
ichiee <iichimura@fastly.com>
2019-04-14 14:22:31 +0100

    first version 2-4.c

--- 2-3.c
+++ 2-3.c
@@ -3,6 +3,8 @@
 #include <math.h>
 #include <ctype.h>
 
+#define MAX 10 /* Maximum number of string */
+
 /*
 Write a function htoi
 which convert strings of hexadecimol digits to(incl optional 0x and 0X)
@@ -19,87 +21,130 @@ rules of hex
 starting with /x or 0x or 0X
 */
 
-
 /*
-
-
 int isxdigi(int c)
 https://www.tutorialspoint.com/c_standard_library/c_function_isxdigit.htm
-
 */
 
-int hextoi (char str1 []);
+
+// also define max number of imput
+// 
+
+
+int hextoi (char str1[]);
 int is_hex (char str1[]);
 
 int main(){
     
-    char str1 [] = "0xa3";
+    // example imput
+    char str1[MAX];
+    str1 = "Xcc1";
     
+    if (hextoi(str1) == -1){
+        return -1;
+    }
     printf ("%d \n", hextoi(str1));
-    
     return 0;
 }
 
+/*
+takes a char[] to convert from hex decemol to equivelant int
+If it contents the invalid charator on the string, it will throw a erorr
 
-// create second finction first
-// both char is hexdesmo without the prefix of indication. this will befinally changed once is_hex function created
-// 
-int hextoi (char str1 []){
+
+// Test to carryed out
+// put str not starting 0x or 0X   ----
+// put str starting 0x              ----
+// put str starting 0X              ----
+
+// put star starting 0x or 0X but none hex charactor ther such as h  ----
+// put empty strings
+// out dufferent data type
+
+
+arg[0]; char[]
+*/ 
+int hextoi (char str1[]){
     int i;
     int len = strlen (str1);
+
     int j = 0;
     int add_all = 0;
 
-    if ((i = is_hex(str1) == 0){
-        printf ("This is not hexdecimol");
+    if ((i = is_hex(str1)) == 0){
+        printf ("This is not hexdecimol \n");
+        return -1;
     }
-    
-    for (i = is_hex(str1); i < len; i++){
-        /* convert from ascii to real number if it is 1-9 (str1 [i] -48) */
-        if (isdigit(str1[i])){
-            j = str1[i] - 48;
+    /*
+    else if ((i = is_hex(str1) == 1){
+        then manupitrate with the string
+
         }
-        else if {
-            /* to simplyfy, make all lower case and convert to hex number in decimol*/
-            j =  tolower (str1[i]);
-            /* convert a-f to 10-15 */
-            j = j - 87;
+    }
+    */
+
+    // if starting with "0x" or "OX", i should be 2
+    else {
+        for (i = is_hex(str1); i < len; i++){
+            /* convert from ascii to real number if it is 1-9 (str1 [i] -48) */
+            if (isdigit(str1[i])){
+                j = str1[i] - 48;
+            }
+            else {
+                /* to simplyfy, make all lower case and convert to hex number in decimol*/
+                j =  tolower (str1[i]);
+
+                if ( j < 97 || j > 102 ){
+                    printf ("Error: This is not valid hexfecimol string, It contents value other than digits, a-f or A-F  \n");
+                    return -1;
+                }
+                /* convert a-f to 10-15 */
+                j = j - 87;
+            }
+            /* convert what it should be power of -len-i-1*/
+            add_all += j * pow (16, len-i-1);
+            printf ("i is %d \n", i);
+            printf ("%d \n", add_all);
         }
-        else {
-            printf ("this van not be valid hexdecimol. It contents value other than 1-9 and a-f or A-F");
-        }
-        /* convert what it should be power of -len-i-1*/
-        add_all += j * pow (16, len-i-1);
-        printf ("i is %d \n", i);
-        printf ("%d \n", add_all);
     }
      //printf ("%d \n", add_all);
      return add_all;
 }
 
+/*
+check if it is starting "\x",  "0x" or "OX"
+if it sterts with "\x" ----- here you need to figure out hopefully does the conversion straight away
+if it starts with "0x" or "0X" then gives 2 for now
+
+
+plan 1: 
+if the \x - then return 1 so that existing function can sort it out
+if 0x or 0X then return 2
+else return 0
+
+plan 2:
+the return need to be string as int can be any of actual number on but we will figure out once we could see 
+
+*/
 int is_hex (char str1[]){
 
     //int i =0;
     int j = 0;
     /*
     if (str1 [0]== '\x'){
-        j = 1;
 
-        // will need impliment ssome exception and error handler
+        // TODO ________impliment to get if '\x' - use library
+        return 1;
     } else */
     if (str1[0] == '0' && (str1[1]== 'x' || str1[1]== 'X')){
-        return j;
+        j = 2;
+        return 2;
     }
     else {
         return 0;
     }
 
 }
-
-
-
-
-
 
 // PLAN ------
 //what need to be done.
---
+++ 2-4.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <string.h>
+
+// #include <typr.h>   // this has isapha but does not need unless you wanna define
+
+/*
+ Write an alternate version of squeeze (s 1,s2) that deletes
+each character in s 1 that matches any character in the string s2.
+*/
+
+#define LIM 10
+#define MAX 20
+//void c_squeeze (char str1[], char str2[]);
+int  get_line1(char a[], char b[]);
+void c_squeeze (char str1[], char str2[]);
+
+int main(){
+
+    char ori_str[LIM];
+    char remove_str[LIM];
+
+    get_line1(ori_str, remove_str);
+    c_squeeze(ori_str, remove_str);
+
+    printf ("After squeezed out :  %s \n", ori_str);
+
+    return 0;
+}
+
+
+/*
+c_squeeze: deletes each character in str1 that matches any character in the string s2
+arg[0]: char array  s１  - original string
+arg[1]: char array  s2  - substring
+*/
+
+void c_squeeze (char str1[], char str2[]){
+
+    int i, j, k, l;
+
+    // cast to int from unsigned long
+    l = (int)strlen(str2);
+    
+        // check each character on str1 until terminator
+        for ( i = j = 0; str1[i] != '\0'; i++){
+            // debug 
+            //printf ("i is %d \n", i);
+            //printf ("j is %d \n", j);
+
+            // check each characotor on s2 until teminator
+            for (k = 0; str2[k] != '\0'; k++){
+                // if 1st matched - get i value on j to be done perfect
+                if (str1[i] == str2[k] && j == 0){
+                    j = i;
+                    break;
+                }        
+                else if( (str1[i] == str2[k]) || (str1[i] != str2[k] && j == 0)){
+                    break;
+                }
+                // this is the last arond and it has not been evicted from the loop yet
+                // this is the one need to mainly copy over 
+                else if (str1[i] != str2[k] && j != 0  && (k == l-1)){                    
+                    str1[j++] = str1[i];
+                }
+            }
+        }
+        str1[j++] = '\0';       
+}
+
+
+/* 
+    getLine:     read a line return int - the length 
+    (changed the name from getline as stdio.h has a declared func with the same name) 
+    arg[0]: char array - input text
+    arg[1]: int -  the maimum limit of the length of input text (arg [0]) 
+*/
+
+int  get_line1(char a[], char b[]){
+    
+    int c, i, k;
+    printf ("Please type the original \n");
+    for (i = 0; i < LIM - 1 && (c = getchar()) != EOF && c != '\n'; i++){       
+        a[i] = c;
+    }
+    // once excuted it will be added as ++i so no need to add extra
+    a[i] = '\0';
+
+    printf("Original Str: %s \n", a);
+
+    printf ("Please type the removing chars \n");
+    for (i = 0; i < LIM - 1 && (k = getchar()) != EOF && k != '\n'; i++){       
+        b[i] = k;
+    }
+    
+    
+    printf("Original Str: %s \n", b);
+
+    return i;
+}
+
