18bfe96ded128048293c5b05dc638d85d533f4d9
ichiee <iichimura@fastly.com>
2019-08-31 15:10:16 +0100

    Cleaned comments

--- 3-2.c
+++ 3-2.c
@@ -3,13 +3,10 @@ Write a function escape (s, t)
 that converts characters like new line and tab into visible escape sequences like
 \n and \t as it copies the string t to s. Use a switch. 
 Write a function for the other direction as well, converting escape sequences into the real characters
- */
+*/
+
 
 /*
-first write regular function that convert that to \t and \n if 
-int i = '\n'
-
-
 C has a variety of integer types: 
 char (at least 8 bits), short (at least 16 bits), 
 int (at least 16 bits), long (at least 32 bits). 
@@ -18,20 +15,14 @@ If you assign a value that is too large to a plain type, the results are undefin
 
 char and int is pretty much the same a part from the six=ze of these
 
-
- A string can be initialized in different ways.
+A string can be initialized in different ways.
 
 1. char str[] = "GeeksforGeeks";
-
 2. char str[50] = "GeeksforGeeks";
-
 3. char str[] = {'G','e','e','k','s','f','o','r','G','e','e','k','s','\0'};
-
 4. char str[14] = {'G','e','e','k','s','f','o','r','G','e','e','k','s','\0'};
 
-when you make a fuction you do mot need define the size of stri (char array) so that you can say char str[]
-
-
+or just simply assign char stri[] = "lalalalala";
 */
 
 #include <stdio.h>
@@ -41,7 +32,6 @@ when you make a fuction you do mot need define the size of stri (char array) so 
 
 void escape_1 (char str[], char strout[]);
 void reverse_escape_1 (char str[], char strout[]);
-
 
 int main (){
 
@@ -83,7 +73,7 @@ void escape_1 (char str[], char strout[]){
 }
 
 void reverse_escape_1 (char str[], char strout[]){
-        int i = 0;
+    int i = 0;
     int n = strlen(str);  
     printf ("strlen is %d \n", n);
     if ( n > MAXNUMBER)
@@ -92,8 +82,7 @@ void reverse_escape_1 (char str[], char strout[]){
     for (int j = 0; j < n; j++){
 
         switch (str[j]){
-            case '\\':
-                
+            case '\\':    
                 j++;
                 switch (str[j]){
                     case 'n': 
@@ -111,7 +100,5 @@ void reverse_escape_1 (char str[], char strout[]){
     }
         strout[i] = '\0';
         printf ("%c", strout[i]);   
-
-
 }
 
